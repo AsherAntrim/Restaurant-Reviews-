5298ccc87c69914f528caf1345193bd74fd248de
Asher <aantrim246@cedarville.edu>
2024-08-23 14:22:04 -0400

    Initial Commit

---
+++ Restaurant ReviewsTests/Restaurant_ReviewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ReviewsTests.swift
+//  Restaurant ReviewsTests
+//
+//  Created by Asher Antrim on 8/23/24.
+//
+
+import Testing
+@testable import Restaurant_Reviews
+
+struct Restaurant_ReviewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
